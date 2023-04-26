fd4ed10bce724ebac3c80334c3b079a0477cc9bd
DavidPMTC <dperalvarezm@edu.tecnocampus.cat>
2023-04-26 16:40:09 +0200

    Create Collision.cs

---
+++ Assets/Scripts/Collision.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    public CameraShake shaker;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        shaker.ShakeCamera(1,1);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
