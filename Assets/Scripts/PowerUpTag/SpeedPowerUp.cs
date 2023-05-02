804dbf8527fee2e18672b1850d930dca97dca363
MarcBarbera <mbarberac@edu.tecnocampus.cat>
2023-05-03 00:26:03 +0200

    minimosArreglosPP

--- Assets/Scripts/PowerUpTag/SpeedPowerUp.cs
+++ Assets/Scripts/PowerUpTag/SpeedPowerUp.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class SpeedPowerUp : MonoBehaviour
 {
+    [SerializeField]
+    float increasedSpeed;
     public void IncreaseSpeed(bool activated, InputMover _inputMover)
     {
        if (activated)
         {
             Destroy(this.gameObject);
             Debug.Log(_inputMover);
-            _inputMover.SpeedBoot(10f);            
+            _inputMover.SpeedBoot(increasedSpeed);            
         }       
     }   
 }
