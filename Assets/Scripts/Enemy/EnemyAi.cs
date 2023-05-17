8fbb96e1da4b72ea03061df0ef3bbb9ec747755a
DavidPMTC <dperalvarezm@edu.tecnocampus.cat>
2023-05-17 19:46:47 +0200

    EnemyAi Implemented

---
+++ Assets/Scripts/Enemy/EnemyAi.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAi : MonoBehaviour
+{
+    public float speed;
+    public float checkRadius;
+    public float attackRadius;
+
+    public bool animationRotate;
+
+    public LayerMask checkPlayer;
+
+    private Transform target;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Vector2 movement;
+    public Vector3 dir;
+
+    private bool isInChaseRange;
+    private bool isInAttackRange;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        target = GameObject.FindWithTag("Player").transform;
+
+    }
+
+    private void Update()
+    {
+        
+    }
+}
