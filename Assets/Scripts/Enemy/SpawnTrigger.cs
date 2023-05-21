deaef1e19541f34e52b91c3c258f54df2628d97c
GerardGerry <gpagesvall@gmail.com>
2023-05-21 21:59:50 +0200

    SpawnTrigger

--- Assets/Scripts/Enemy/SpawnTrigger.cs
+++ Assets/Scripts/Enemy/SpawnTrigger.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class SpawnTrigger : MonoBehaviour
 {
-    EnemySpawner spawn;
+    EnemySpawner spawn1;
+    EnemySpawner spawn2;
+    EnemySpawner spawn3;
+    EnemySpawner spawn4;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        var createEnemies = collision.GetComponent<ItSpawns>();
-        if (collision.GetComponent<EnemySpawner>())
-        {
-            Debug.Log("Spawning enemies");
-            createEnemies.ActivateSpawn();
-        }
+        spawn1 = GameObject.Find("Spawner_1").GetComponentInChildren<EnemySpawner>();
+        spawn2 = GameObject.Find("Spawner_2").GetComponentInChildren<EnemySpawner>();
+        spawn3 = GameObject.Find("Spawner_3").GetComponentInChildren<EnemySpawner>();
+        spawn4 = GameObject.Find("Spawner_4").GetComponentInChildren<EnemySpawner>();
+
+        Debug.Log("Spawning enemies");
+            spawn1.ActivateSpawn();
+            spawn2.ActivateSpawn();
+            spawn3.ActivateSpawn();
+            spawn4.ActivateSpawn();
+       
     }
     
 }
