fd2dec2782d3cff9da66e9760a93c76dab008791
DavidPMTC <dperalvarezm@edu.tecnocampus.cat>
2023-05-22 18:39:45 +0200

    switch branch

---
+++ Assets/Scripts/Enemy/ScriptMenuClasse.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptMenuClasse : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
+/*
+  The canvas:
+ - Root object for all UI elements, like a canvas where you can stick UI elements on it.
+
+  Canvas scaling:
+- Constant Pixel Size:
+- Scale with Sceen Size (recommended): You have to decide the size difference.
+
+
+
+ Score nota: Free Spect, mirar que no es descuadri del tot el menu al escalar (perfecte es impossible)
+ 
+
+ Nota escalable menus -> Model View Controller: Model -> Valor punts de vida View -> El que mostres Controller -> El que et permet cambiar-ho
+
+
+ UI Juice 
+
+ Animations good :) 
+
+
+ */
