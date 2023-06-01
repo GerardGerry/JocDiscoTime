7ef06930d69201d1e974268d2747582d25f93015
GerardGerry <gpagesvall@gmail.com>
2023-06-01 22:19:03 +0200

    BleedingParticle

--- Assets/Scripts/PlayerScripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerScripts/PlayerHealth.cs
@@ -9,7 +9,8 @@ public class PlayerHealth : MonoBehaviour, ItTakesDamage, ItHeals
     [Range (0, 8)]
     private int m_currentHearts = 6;
     PlayerController _playerController;
-    [SerializeField] private ParticleSystem particles;
+    [SerializeField] private ParticleSystem healingParticles;
+    [SerializeField] private ParticleSystem BleedingParticles;
 
     [SerializeField]
     HUD hud;
@@ -30,12 +31,17 @@ public class PlayerHealth : MonoBehaviour, ItTakesDamage, ItHeals
         m_currentHearts++;
         CheckAmount(m_currentHearts);
         hud.SetHealth(m_currentHearts);
-        particles.Play();
+        healingParticles.Play();
     }
     private void CheckAmount(int amount)
     {
         if (amount < 0) { m_currentHearts = 0; }
         else if (amount > 8) { m_currentHearts = 8; }
-        else if (amount < 3) { _playerController.PlayerHealthLow(amount); }
+        else if(amount == 3) { BleedingParticles.Stop(); }
+        else if (amount < 3) 
+        { 
+            _playerController.PlayerHealthLow(amount);
+            BleedingParticles.Play();
+        }
     }
 }
